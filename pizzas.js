23753de6034b04594dbda4f319fd26888f7bcce0
Andre Pacola <andreluizzpacolla@outlook.com>
2023-08-13 21:48:24 -0300

    first commit

---
+++ pizzas.js
@@ -0,0 +1,10 @@
+let pizzaJson = [
+    {id:1, name:'Muçarela ', img:'images/pizza.png', price:50.19, sizes:['100g', '530g', '860g'], description:'È coberta com molho de tomate, queijo tipo muçarela , azeitonas pretas e orégano e massa com fermentação natural'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:68.00, sizes:['320g', '530g', '860g'], description:'Especialmente preparada com recheio generoso de linguiça calabresa de primeira qualidade, queijo muçarela e orégano.'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:57.45, sizes:['320g', '530g', '860g'], description:' 4 tipos de queijos especial. muçarela , Provolone, Parmesão, Catupiry, Cheddar, Gorgonzola e Orégano.'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:49.77, sizes:['320g', '530g', '860g'], description:'Vem muçarela , pepperoni , brócolis e orégano para você preparar e surpreender a família. Confira essa delícia agora mesmo!'},
+    {id:5, name:'Moda da Casa', img:'images/pizza6.png', price:68.55, sizes:['320g', '530g', '860g'], description:'Vem queijo tipo mussarela ,presunto fatiado,calabresa defumada fatiada,bacom feito,milho , catupiry e orégano !  '},
+    {id:6, name:'Frango com Catupiry', img:'images/pizza9.webp', price:52.36, sizes:['320g', '530g', '860g'], description:'Frango Caipira ,muçarela  , molho  de tomate especial, orégano , tomate frescos ,azeitonas'},
+    {id:7, name:'Sorvete', img:'images/pizza5.png', price:51.43, sizes:['320g', '530g', '860g'], description:'Pizza de brigadeiro com sorvete e borda recheada de doce de leite para você servir de sobremesa e animar a festa! '},
+    {id:8, name:'Chocolate', img:'images/pizza7.png', price:62.36, sizes:['320g', '530g', '860g'], description:' Pizza de Chocolate com Confete deliciosos feita com Cobertura Chocolate Meio Amargo.'}
+];
